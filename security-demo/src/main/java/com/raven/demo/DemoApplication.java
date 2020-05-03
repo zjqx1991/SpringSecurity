b61b30bbffbaff69007916f6dfc7fb6d4c374e35
紫荆秋雪 <zjqx1991@163.com>
2020-05-03 17:39:24 +0800

    加入SpringSecurity测试通过

---
+++ security-demo/src/main/java/com/raven/demo/DemoApplication.java
@@ -0,0 +1,18 @@
+package com.raven.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan("com.raven")
+public class DemoApplication {
+    public static void main(String[] args) {
+        try {
+            SpringApplication.run(DemoApplication.class, args);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
