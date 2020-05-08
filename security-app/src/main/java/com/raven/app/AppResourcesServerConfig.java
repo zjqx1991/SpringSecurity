61400a8de5e30e02aedfc03418b551a032f40d7f
紫荆秋雪 <zjqx1991@163.com>
2020-05-08 12:05:51 +0800

    App模块准备

---
+++ security-app/src/main/java/com/raven/app/AppResourcesServerConfig.java
@@ -0,0 +1,10 @@
+package com.raven.app;
+
+
+/**
+ * 资源服务器配置
+ */
+//@Configuration
+//@EnableResourceServer
+public class AppResourcesServerConfig {
+}
---
+++ security-app/src/main/java/com/raven/app/authentication/AppAuthenticationFailureHandler.java
@@ -0,0 +1,46 @@
+package com.raven.app.authentication;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.raven.core.enums.RavenLoginType;
+import com.raven.core.properties.RavenSecurityProperties;
+import com.raven.core.response.RavenR;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class AppAuthenticationFailureHandler extends SimpleUrlAuthenticationFailureHandler {
+
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Autowired
+    private ObjectMapper objectMapper;
+    @Autowired
+    private RavenSecurityProperties securityProperties;
+
+
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+
+        logger.info("自定义失败处理器");
+
+        if (RavenLoginType.JSON.equals(this.securityProperties.getBrowser().getLoginType())) {
+
+            response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+            response.setContentType("application/json;charset=UTF-8");
+            response.getWriter().write(this.objectMapper.writeValueAsString(new RavenR(exception.getMessage())));
+        }
+        else {
+            super.onAuthenticationFailure(request, response, exception);
+        }
+    }
+
+}
